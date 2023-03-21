20c3ddad4a812703a1694c72ac32f748a264182d
AndrewSepic <andrew@thinkupdesign.ca>
2023-03-21 17:23:25 -0400

    Added styles to homepage locations & fixed order online links

--- homepagenew.php
+++ homepagenew.php
@@ -33,7 +33,7 @@ get_header(); ?>
 							</div>
 							<?php
 								if($online_order): ?>
-									<a class="online-order" href="<? $online_order['url']; ?>" target="_blank">Order Online</a>
+									<a class="online-order" href="<?= $online_order['url']; ?>" target="<?= $online_order['target'] ?>"><?= $online_order['title']?></a>
 								<? endif; ?>
 						</div>
 						<?php
